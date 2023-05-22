3a3f341a4b3c121eaa048135c5558c1b925949e8
Justin118 <nice.justin@gmail.com>
2023-05-22 20:40:36 +0900

    first commit test

---
+++ c/test1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("hello world!\n");
+    printf("testtest\n");
+    return 0;
+}
---
+++ java/HelloWorldApp.java
@@ -0,0 +1,7 @@
+// package java;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("hello world! Jiseoung~~dddddd");
+    }
+}
